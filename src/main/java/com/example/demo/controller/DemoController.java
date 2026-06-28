9c283cd1f60f2f87d321078f26935b1cecce5c01
Jerret Halter <jeehalte@users.noreply.github.com>
2026-06-28 10:48:04 -0600

    Update DemoController to return list of ServiceResult

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ServiceResult;
 import com.example.demo.facade.DemoFacade;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 public class DemoController {
@@ -15,7 +16,7 @@ public class DemoController {
   }
 
   @GetMapping("/demo")
-  public Map<String, Object> demo() {
+  public List<ServiceResult> demo() {
     return facade.handle();
   }
 }
